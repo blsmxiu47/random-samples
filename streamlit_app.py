811cea7b83713a24bc8b1165b7cbf5340e0f563a
Wes Warriner <fridgehistory@gmail.com>
2022-04-05 14:50:32 -0700

    refine user controls and set slider defaults

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,18 @@ def main():
     st.subheader("Distribution")
     distribution = st.selectbox("Select distribution", ("Gaussian", "Beta"))
 
-    sample_size = st.slider("Select sample size", 1, 1000) # can also be an enter-a-number srt of deal
+    sample_size = st.slider("Select sample size", 1, 1000, 100) # can also be an enter-a-number srt of deal
 
     st.subheader("Parameters")
     if distribution == "Gaussian":
-        mu = st.slider("\u03BC (Mean)", -10, 10)
-        sigma_squared = st.slider("\u03C3\u00B2 (Variance)", 0.01, 10.0)
+        mu = st.slider("\u03BC (Mean)", -10, 10, 0)
+        sigma_squared = st.slider("\u03C3\u00B2 (Variance)", 0.01, 10.0, 1.0)
+
+        # Base
+        x = np.linspace(norm.ppf(0.0001, mu, sigma_squared), norm.ppf(0.9999, mu, sigma_squared), 100)
+        df = pd.DataFrame({'x': x, 'f(x)': norm.pdf(x, mu, sigma_squared)})
 
         # Normal pdf line chart
-        x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
-        df = pd.DataFrame({'x': x, 'f(x)': norm.pdf(x, mu, sigma_squared)})
         norm_pdf_line = alt.Chart(df).mark_line().encode(
             x='x',
             y='f(x)'
@@ -47,18 +49,22 @@ def main():
         norm_pdf_chart = alt.layer(
             norm_pdf_line, selectors, points, rules, text
         )
+        st.latex("PDF\\ of\\ \mathcal{N}"+f"({mu}, {sigma_squared})")
         st.altair_chart(norm_pdf_chart, use_container_width=True)
         
         # Histogram of random sample
-        sample = pd.DataFrame(
-            norm.rvs(loc=mu, scale=sigma_squared, size=sample_size), columns=['vals']
-        )
+        sample = pd.DataFrame(norm.rvs(loc=mu, scale=sigma_squared, size=sample_size), columns=['vals'])
         base = alt.Chart(sample)
+
+        max_bins = st.slider("Max histogram bins", 5, 40, 20)
         sample_hist = base.mark_bar().encode(
-            x=alt.X('vals:Q', bin={"step": 1}),
+            x=alt.X('vals:Q', bin={"maxbins": max_bins}),
             y='count()'
         )
         st.altair_chart(sample_hist, use_container_width=True)
+
+        # combined = alt.layer(norm_pdf_chart, sample_hist)
+        # st.altair_chart(combined, use_container_width=True)
 
     elif distribution == "Beta":
         alpha = st.slider("\u03B1", 0.01, 100.00)
