6c9a8a198bfea9373db602692ac5afaa7f9158de
Wes Warriner <fridgehistory@gmail.com>
2022-04-22 17:20:10 -0700

    improve app.py legibility

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,33 +6,79 @@ import streamlit as st
 
 
 def generate_altair_pdf(df):
-    pdf_line = alt.Chart(df).mark_line().encode(
-        x='x',
-        y='f(x)'
+    pdf_line = (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x='x',
+            y='f(x)'
+        )
     )
+
     nearest = alt.selection(
-        type='single', nearest=True, on='mouseover', 
-        fields=['x'], empty='none'
+        type='single', 
+        nearest=True, 
+        on='mouseover', 
+        fields=['x'], 
+        empty='none'
     )
-    selectors = alt.Chart(df).mark_point().encode(
-        x='x:Q',
-        opacity=alt.value(0),
-    ).add_selection(
-        nearest
+
+    selectors = (
+        alt.Chart(df)
+        .mark_point()
+        .encode(
+            x='x:Q',
+            opacity=alt.value(0),
+        )
+        .add_selection(nearest)
     )
-    points = pdf_line.mark_point().encode(
-        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+
+    points = (
+        pdf_line
+        .mark_point()
+        .encode(
+            opacity=(
+                alt.condition(
+                    nearest, 
+                    alt.value(1), 
+                    alt.value(0)
+                )
+            )
+        )
     )
-    text = pdf_line.mark_text(align='left', dx=5, dy=-5, color='white').encode(
-        text=alt.condition(nearest, 'f(x):Q', alt.value(' '))
+
+    text = (
+        pdf_line
+        .mark_text(
+            align='left', 
+            dx=5, 
+            dy=-5, 
+            color='white'
+        )
+        .encode(
+            text=(
+                alt.condition(
+                    nearest, 
+                    'f(x):Q', 
+                    alt.value(' ')
+                )
+            )
+        )
     )
-    rules = alt.Chart(df).mark_rule(color='gray').encode(
-        x='x:Q',
-    ).transform_filter(
-        nearest
+
+    rules = (
+        alt.Chart(df)
+        .mark_rule(color='gray')
+        .encode(x='x:Q',)
+        .transform_filter(nearest)
     )
+
     pdf_chart = alt.layer(
-        pdf_line, selectors, points, rules, text
+        pdf_line, 
+        selectors, 
+        points, 
+        rules, 
+        text
     )
 
     return pdf_chart
@@ -40,16 +86,30 @@ def generate_altair_pdf(df):
 
 def generate_altair_sample_hist(sample):
     base = alt.Chart(sample)
+
     max_bins = st.slider("Max histogram bins", 5, 40, 40)
-    sample_hist = base.mark_bar().encode(
-        x=alt.X('vals:Q', bin={"maxbins": max_bins}),
-        y='count()'
-    ).properties(
-        title=f'Random Sample (n = {len(sample)})'
+
+    sample_hist = (
+        base
+        .mark_bar()
+        .encode(
+            x=alt.X(
+                'vals:Q', 
+                bin={"maxbins": max_bins}
+            ),
+            y='count()'
+        )
+        .properties(
+            title=f'Random Sample (n = {len(sample)})'
+        )
     )
-    sample_hist = sample_hist.configure_title(
-        fontSize=20,
-        font='Courier New'
+
+    sample_hist = (
+        sample_hist
+        .configure_title(
+            fontSize=20,
+            font='Courier New'
+        )
     )
 
     return sample_hist
@@ -57,79 +117,170 @@ def generate_altair_sample_hist(sample):
 
 def main():
     st.sidebar.subheader("Distribution")
-    distribution = st.sidebar.selectbox("Select distribution", ("Gaussian", "Beta", "Exponential"))
+    distribution = st.sidebar.selectbox(
+        label="Select distribution", 
+        options=("Gaussian", "Beta", "Exponential")
+    )
 
-    sample_size = st.sidebar.slider("Select sample size", 1, 10000, 1000) # can also be an enter-a-number srt of deal
-
+    sample_size = st.sidebar.slider(
+        label="Select sample size", 
+        min_value=1, 
+        max_value=10000, 
+        value=1000
+    ) # can also be an enter-a-number srt of deal
+ 
     st.sidebar.subheader("Parameters")
     if distribution == "Gaussian":
         # Gaussian parameters
         input_type = st.sidebar.radio(
             "Input Type",
-            ('Sliders', 'Enter Values'))
+            ('Sliders', 'Enter Values')
+        )
         if input_type == 'Sliders':
-            mu = st.sidebar.slider("\u03BC (Mean)", -10.0, 10.0, 0.0)
-            sigma_squared = st.sidebar.slider("\u03C3\u00B2 (Variance)", 0.01, 10.0, 1.0)
+            mu = st.sidebar.slider(
+                label="\u03BC (Mean)", 
+                min_value=-10.0, 
+                max_value=10.0, 
+                value=0.0
+            )
+            sigma_squared = st.sidebar.slider(
+                label="\u03C3\u00B2 (Variance)", 
+                min_value=0.01, 
+                max_value=10.0, 
+                value=1.0
+            )
         elif input_type == 'Enter Values':
-            mu = st.sidebar.number_input("\u03BC (Mean)", value=0.0, step=1.0)
-            sigma_squared = st.sidebar.number_input("\u03C3\u00B2 (Variance)", min_value=0.01, value=1.0, step=0.1)
+            mu = st.sidebar.number_input(
+                label="\u03BC (Mean)", 
+                value=0.0, 
+                step=1.0
+            )
+            sigma_squared = st.sidebar.number_input(
+                label="\u03C3\u00B2 (Variance)", 
+                min_value=0.01, 
+                value=1.0, 
+                step=0.1
+            )
 
         # PDF
-        x = np.linspace(norm.ppf(0.0001, mu, sigma_squared), norm.ppf(0.9999, mu, sigma_squared), 100)
-        df = pd.DataFrame({'x': x, 'f(x)': norm.pdf(x, mu, sigma_squared)})
+        x = np.linspace(
+            norm.ppf(0.0001, mu, sigma_squared), 
+            norm.ppf(0.9999, mu, sigma_squared),
+            100
+        )
+        df = pd.DataFrame({
+            'x': x, 
+            'f(x)': norm.pdf(x, mu, sigma_squared)
+        })
         # Normal pdf line chart
         norm_pdf_chart = generate_altair_pdf(df)
         # Display in app
         st.latex("PDF\\ of\\ \mathcal{N}"+f"({np.round(mu, 2)}, {np.round(sigma_squared, 2)})")
-        st.altair_chart(norm_pdf_chart, use_container_width=True)
+        st.altair_chart(
+            norm_pdf_chart, 
+            use_container_width=True
+        )
         
         # Random sample
-        sample = pd.DataFrame(norm.rvs(loc=mu, scale=sigma_squared, size=sample_size), columns=['vals'])
+        sample = pd.DataFrame(
+            norm.rvs(loc=mu, scale=sigma_squared, size=sample_size), 
+            columns=['vals']
+        )
         # Sample histogram
         sample_hist = generate_altair_sample_hist(sample)
         # Display in app
-        st.altair_chart(sample_hist, use_container_width=True)
+        st.altair_chart(
+            sample_hist, 
+            use_container_width=True
+        )
 
     elif distribution == "Beta":
         # Beta parameters
-        a = st.sidebar.slider("\u03B1", 0.01, 10.0, 2.0)
-        b = st.sidebar.slider("\u03B2", 0.01, 10.0, 5.0)
+        a = st.sidebar.slider(
+            label="\u03B1", 
+            min_value=0.01, 
+            max_value=10.0, 
+            value=2.0
+        )
+        b = st.sidebar.slider(
+            label="\u03B2", 
+            min_value=0.01, 
+            max_value=10.0, 
+            value=5.0
+        )
 
         # PDF
-        x = np.linspace(beta.ppf(0.0001, a, b), beta.ppf(0.9999, a, b), 100)
-        df = pd.DataFrame({'x': x, 'f(x)': beta.pdf(x, a, b)})
+        x = np.linspace(
+            beta.ppf(0.0001, a, b), 
+            beta.ppf(0.9999, a, b), 
+            100
+        )
+        df = pd.DataFrame({
+            'x': x, 
+            'f(x)': beta.pdf(x, a, b)
+        })
         # Beta pdf line chart
         beta_pdf_chart = generate_altair_pdf(df)
         # Display in app
         st.latex("PDF\\ of\\ \mathcal{Beta}"+f"({a}, {b})")
-        st.altair_chart(beta_pdf_chart, use_container_width=True)
+        st.altair_chart(
+            beta_pdf_chart, 
+            use_container_width=True
+        )
 
         # Random sample
-        sample = pd.DataFrame(beta.rvs(a, b, size=sample_size), columns=['vals'])
+        sample = pd.DataFrame(
+            beta.rvs(a, b, size=sample_size), 
+            columns=['vals']
+        )
         # Sample histogram
         sample_hist = generate_altair_sample_hist(sample)
         # Display in app
-        st.altair_chart(sample_hist, use_container_width=True)
+        st.altair_chart(
+            sample_hist, 
+            use_container_width=True
+        )
 
     elif distribution == "Exponential":
         # Beta parameters
-        l = st.sidebar.slider("\u03BB", 0.01, 10.0, 1.0)
+        l = st.sidebar.slider(
+            label="\u03BB", 
+            min_value=0.01, 
+            max_value=10.0, 
+            value=1.0
+        )
 
         # PDF
-        x = np.linspace(expon.ppf(0.0001, scale=1/l), expon.ppf(0.9999, scale=1/l), 100)
-        df = pd.DataFrame({'x': x, 'f(x)': expon.pdf(x, scale=1/l)})
+        x = np.linspace(
+            expon.ppf(0.0001, scale=1/l), 
+            expon.ppf(0.9999, scale=1/l), 
+            100
+        )
+        df = pd.DataFrame({
+            'x': x, 
+            'f(x)': expon.pdf(x, scale=1/l)
+        })
         # Beta pdf line chart
         expon_pdf_chart = generate_altair_pdf(df)
         # Display in app
         st.latex("PDF\\ of\\ Exp"+f"({l})")
-        st.altair_chart(expon_pdf_chart, use_container_width=True)
+        st.altair_chart(
+            expon_pdf_chart, 
+            use_container_width=True
+        )
 
         # Random sample
-        sample = pd.DataFrame(expon.rvs(scale=1/l, size=sample_size), columns=['vals'])
+        sample = pd.DataFrame(
+            expon.rvs(scale=1/l, size=sample_size), 
+            columns=['vals']
+        )
         # Sample histogram
         sample_hist = generate_altair_sample_hist(sample)
         # Display in app
-        st.altair_chart(sample_hist, use_container_width=True)
+        st.altair_chart(
+            sample_hist, 
+            use_container_width=True
+        )
 
 
 st.sidebar.title("Random Samples")
